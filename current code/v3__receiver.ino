93a8485cb14fb3ee15a31e799ef4d68a0b7f3c84
Agneya-1BM22CS024 <154543606+Agneya-1BM22CS024@users.noreply.github.com>
2025-10-26 13:03:08 +0530

    Create v3__receiver.ino

---
+++ current code/v3__receiver.ino
@@ -0,0 +1,42 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+#define LORA_SS 10
+#define LORA_RST 9
+#define LORA_DIO0 2
+#define LORA_FREQUENCY 433.2E6
+#define LORA_SYNC_WORD 0x34
+#define LORA_SPREADING_FACTOR 10
+#define LORA_SIGNAL_BANDWIDTH 125E3
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
+  if (!LoRa.begin(LORA_FREQUENCY)) { while (1); }
+  LoRa.setSyncWord(LORA_SYNC_WORD);
+  LoRa.setSpreadingFactor(LORA_SPREADING_FACTOR);
+  LoRa.setSignalBandwidth(LORA_SIGNAL_BANDWIDTH);
+  Serial.println("Receiver Ready");
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    String receivedString = "";
+    while (LoRa.available())
+      receivedString += (char)LoRa.read();
+    Serial.println(receivedString);
+  }
+
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+    if (command.equalsIgnoreCase("START") || command.equalsIgnoreCase("STOP")) {
+      LoRa.beginPacket();
+      LoRa.print(command);
+      LoRa.endPacket();
+      Serial.println("Command sent: " + command);
+    }
+  }
+}
